a4e5d94c7b46cf3f69fef2bce5c1f9137a361cf3
Benjamin Huang <bhuang@rms.com>
2019-12-12 23:18:43 -0800

    create project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # springboot-kafka-example
+
+## Reference
+- https://www.youtube.com/watch?v=NjHYWEV_E_o
+    - https://github.com/TechPrimers/spring-boot-kafka-producer-example
---
+++ src/test/java/cn/ben/springbootkafkaexample/SpringbootKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ben.springbootkafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
